2d4b17bb255b3132f8a46528c390831a93562770
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-16 17:11:34 +0000

    Release 0.9.0

--- src/ittybit/core/client_wrapper.py
+++ src/ittybit/core/client_wrapper.py
@@ -24,10 +24,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "ittybit/0.8.15",
+            "User-Agent": "ittybit/0.9.0",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "ittybit",
-            "X-Fern-SDK-Version": "0.8.15",
+            "X-Fern-SDK-Version": "0.9.0",
             **(self.get_custom_headers() or {}),
         }
         if self._version is not None:
